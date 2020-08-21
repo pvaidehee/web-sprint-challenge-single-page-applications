127095bc6796c9528d62464717757664f6e79342
vaidehee patel <pvaidehee@gmail.com>
2020-08-21 22:10:54 +0530

    still doing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React,{useState} from "react";
+import {Route} from 'react-router-dom';
+
+import Home from "./Pages/Home";
+import Form from "./Pages/form";
+import OrderComplete from './Pages/OrderCompletePage';
 
 const App = () => {
+  const [order,setOrder] = useState([]);
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <>
+          <Route exact path ='/'>
+              <Home />
+          </Route>
+
+          <Route path='/pizza'>
+              <Form order = {order} setOrder = {setOrder} />
+          </Route>
+
+          <Route path='/order'>
+              <OrderComplete order = {order} />
+          </Route>
+
+
+      </>
   );
 };
 export default App;
---
+++ src/Pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Route} from 'react-router-dom';
+import Header from "../Components/Header";
+
+
+export default function Home(props){
+    return(
+        <>
+        <Header />
+        </>
+    )
+}
